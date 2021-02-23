817053297fad9d4351c0a469665100e67abb1e5a
Diogo Souza <diogo.souza@uds.com.br>
2021-02-22 22:02:42 -0400

    adicionado server do swagger

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { Logger } from '@nestjs/common';
-import { AppModule } from './app.module';
-import { apiConfig, apiServer } from './servers';
+import { AppModule } from '@/app.module';
+import { apiConfig, apiServer, swaggerConfig, swaggerServer } from '@/servers';
 
 async function bootstrap() {
   const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
 
   apiServer(app);
+  swaggerServer(app);
 
   const { host, port, prefix: apiPrefix } = apiConfig;
+  const { prefix: swaggerPrefix } = swaggerConfig;
   await app.listen(port);
 
   logger.debug(`Appliation listening on: http://${host}:${port}/${apiPrefix}`);
+  logger.debug(`Appliation listening on: http://${host}:${port}/${swaggerPrefix}`);
 }
 bootstrap();
